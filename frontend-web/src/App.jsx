79990d5bbb0e94d6e0a17b3b85d73eaf37d50cc6
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-12-02 10:06:28 +0700

    Update app.jsx

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -5,7 +5,6 @@ import viVN from 'antd/locale/vi_VN';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/user/DashboardPage';
-import TransactionPage from './pages/user/TransactionPage';
 import NotificationPage from './pages/user/NotificationPage';
 import SettingsPage from './pages/user/SettingsPage';
 import ChangePasswordPage from './pages/user/ChangePasswordPage';
@@ -18,8 +17,6 @@ import OnboardingStep2 from './pages/user/OnboardingStep2';
 import OnboardingStep3 from './pages/user/OnboardingStep3';
 // Admin Pages
 import AdminDashboardPage from './pages/admin/AdminDashboardPage';
-import ContentManagementPage from './pages/admin/ContentManagementPage';
-import AIModelManagementPage from './pages/admin/AIModelManagementPage';
 import authService from './services/authService';
 import { SidebarProvider } from './contexts/SidebarContext';
 
@@ -67,14 +64,6 @@ function App() {
             }
           />
           
-          <Route
-            path="/transactions/add"
-            element={
-              <ProtectedRoute>
-                <TransactionPage />
-              </ProtectedRoute>
-            }
-          />
           
           <Route
             path="/notifications"
@@ -132,22 +121,7 @@ function App() {
               </AdminRoute>
             }
           />
-          <Route
-            path="/admin/content"
-            element={
-              <AdminRoute>
-                <ContentManagementPage />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/ai-models"
-            element={
-              <AdminRoute>
-                <AIModelManagementPage />
-              </AdminRoute>
-            }
-          />
+          
           <Route
             path="/admin/*"
             element={
