e6a89915ac297a1e6f59196d3e4ca334b6def5e1
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-12-07 21:56:39 +0700

    [Feature] Login screen and related UI components

---
+++ finpal_mobile/lib/core/widgets/custom_button.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../constants/app_colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final bool isGradient;
+  final Color? backgroundColor;
+  final Color? textColor;
+  final double? fontSize;
+  final double? height;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.isGradient = true,
+    this.backgroundColor,
+    this.textColor,
+    this.fontSize = 14,
+    this.height = 48,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      decoration: BoxDecoration(
+        gradient: isGradient
+            ? const LinearGradient(
+                colors: [
+                  AppColors.primaryGradientStart,
+                  AppColors.primaryGradientEnd,
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              )
+            : null,
+        color: !isGradient ? (backgroundColor ?? AppColors.primary) : null,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: onPressed,
+          borderRadius: BorderRadius.circular(8),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: textColor ?? AppColors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
