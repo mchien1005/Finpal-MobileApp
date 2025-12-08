6b827a05a73906681ca1030d6ecc020862937c26
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-12-08 15:58:43 +0700

    Folder Flutter

--- backendAI/app/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Finpal AI Backend - Machine Learning Module
-"""
-
-__version__ = "1.0.0"
--- backendAI/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-ML Models Package
-"""
-
-from .categorization import TransactionCategorizer
-from .anomaly_detection import AnomalyDetector
-from .spending_prediction import SpendingPredictor
-
-__all__ = ["TransactionCategorizer", "AnomalyDetector", "SpendingPredictor"]
