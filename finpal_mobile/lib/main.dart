d284c451545780427a03214c1c1e02d2cfe81779
cr7coder <cr719852004@gmail.com>
2025-12-09 22:05:24 +0700

    [feature]dashboard screen

--- finpal_mobile/lib/main.dart
+++ finpal_mobile/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'presentation/screens/auth/login_screen.dart';
-
+import 'package:finpal_mobile/presentation/screens/home/dashboard_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyApp(),
+      home: const DashboardScreen(),
     );
   }
 }
