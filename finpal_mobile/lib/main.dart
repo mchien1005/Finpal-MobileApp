35660fcf6bf0186cf1d16f646eef192d9fc282ae
cr7coder <cr719852004@gmail.com>
2025-12-09 22:12:12 +0700

    [Update] Change home screen from Dashboard to Login

--- finpal_mobile/lib/main.dart
+++ finpal_mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:finpal_mobile/presentation/screens/home/dashboard_screen.dart';
+// import 'package:finpal_mobile/presentation/screens/home/dashboard_screen.dart';
+import 'presentation/screens/auth/login_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const DashboardScreen(),
+      home: const LoginScreen(),
     );
   }
 }
