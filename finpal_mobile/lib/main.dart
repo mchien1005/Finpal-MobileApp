45a082ffd75b4cf6429750c569bfb7b2bc699a17
cr7coder <cr719852004@gmail.com>
2025-12-12 20:09:41 +0700

    [feature]Change passrord

--- finpal_mobile/lib/main.dart
+++ finpal_mobile/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:finpal_mobile/presentation/screens/home/dashboard_screen.dart';
 import 'presentation/screens/auth/login_screen.dart';
+import 'package:finpal_mobile/presentation/screens/transactions/transactions_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const MyApp(),
     );
   }
 }
