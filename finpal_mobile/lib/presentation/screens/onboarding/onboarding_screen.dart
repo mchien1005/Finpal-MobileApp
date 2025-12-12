aa25d2117d0ce17f88bb795c6478972e9deca49d
mchien1005 <167665286+mchien1005@users.noreply.github.com>
2025-12-12 19:50:19 +0700

    [Update] Improve error handling and connectivity checks in registration

--- finpal_mobile/lib/presentation/screens/onboarding/onboarding_screen.dart
+++ finpal_mobile/lib/presentation/screens/onboarding/onboarding_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../../core/constants/app_colors.dart';
-import '../../screens/home/dashboard_screen.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
